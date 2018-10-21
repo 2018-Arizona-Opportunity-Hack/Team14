5b8a204638452d26ce90d2569f49c7a067bf24c5
Raghavhub <rragvendran@gmail.com>
2018-10-20 23:11:16 -0700

    added SQL interface

--- server.js
+++ server.js
@@ -1,14 +1,30 @@
 const express = require('express');
 const path = require('path');
 var cors = require('cors')
-
+var http = require('http')
 const app = express();
 const port = process.env.PORT || 5000;
+var connection  = require('express-myconnection'); 
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "password",
+  database:"db"
+});
+
 
 app.use(cors()) // Use this after the variable declaration
 // API calls
-app.get('/', (req, res) => {
-  res.send({ express: 'Welcome to pet animals!' });
+// app.get('/', (req, res) => {
+//   res.send({ express: 'Welcome to pet animals!' });
+// });
+
+app.get('/', function(req, res, next) {
+ 	connection.query('SELECT * from users', function (error, results, fields) {
+		if (error) throw error;
+		res.send(JSON.stringify(results));
+	});
 });
 
 app.get('/pet', (req, res) => {
